6e87c6a9b018db713b302918f85a4c912cb31718
suhaikha <suhail.khan.ftg@gmail.com>
2022-12-06 15:17:23 +0530

    added volley vpn changes

--- app/src/main/java/com/lazycoder/cakevpn/view/MainActivity.java
+++ app/src/main/java/com/lazycoder/cakevpn/view/MainActivity.java
@@ -14,6 +14,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.lazycoder.cakevpn.R;
 import com.lazycoder.cakevpn.adapter.ServerListRVAdapter;
 import com.lazycoder.cakevpn.interfaces.ChangeServer;
@@ -23,6 +29,10 @@ import com.lazycoder.cakevpn.model.Server;
 import java.util.ArrayList;
 
 import com.lazycoder.cakevpn.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity implements NavItemClickListener {
@@ -106,26 +116,72 @@ public class MainActivity extends AppCompatActivity implements NavItemClickListe
      */
     private ArrayList getServerList() {
 
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String getApiUrl="http://43.205.217.144:8080/vpns";
         ArrayList<Server> servers = new ArrayList<>();
 
-        servers.add(new Server("United States",
-                Utils.getImgURL(R.drawable.usa_flag),
-                "us.ovpn",
-                "freeopenvpn",
-                "416248023"
-        ));
-        servers.add(new Server("Japan",
-                Utils.getImgURL(R.drawable.japan),
-                "japan.ovpn",
-                "vpn",
-                "vpn"
-        ));
-        servers.add(new Server("Sweden",
-                Utils.getImgURL(R.drawable.sweden),
-                "sweden.ovpn",
-                "vpn",
-                "vpn"
-        ));
+
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, getApiUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        JSONArray serverArray= null;
+                        try {
+                            serverArray = new JSONArray(response);
+                            for(int i=0;i<serverArray.length();i++){
+
+                                JSONObject json_data = serverArray.getJSONObject(i);
+                                String name =json_data.getString("file_name");
+                                //String surname =json_data.getString("surname");
+                                System.out.println(name+"suhail");
+                                servers.add(new Server("United States",
+                                        Utils.getImgURL(R.drawable.usa_flag),
+                                        name,
+                                        "vpn",
+                                        "vpn"
+                                ));
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println("That work!");
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
+//        servers.add(new Server("United States",
+//                Utils.getImgURL(R.drawable.usa_flag),
+//                "us.ovpn",
+//                "freeopenvpn",
+//                "416248023"
+//        ));
+//        servers.add(new Server("Japan",
+//                Utils.getImgURL(R.drawable.japan),
+//                "japan.ovpn",
+//                "vpn",
+//                "vpn"
+//        ));
+//        servers.add(new Server("Sweden",
+//                Utils.getImgURL(R.drawable.sweden),
+//                "sweden.ovpn",
+//                "vpn",
+//                "vpn"
+//        ));
         servers.add(new Server("Korea",
                 Utils.getImgURL(R.drawable.korea),
                 "korea.ovpn",
